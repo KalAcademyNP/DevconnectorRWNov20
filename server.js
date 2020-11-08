24ec1b4b0610c9bd78bf7327032791f17b323ef9
Kal <info@kalacademy.org>
2020-11-08 14:58:44 -0800

    Models and routes for user

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const app = express();
+
+const users = require('./routes/api/users');
+
+//Body parser config
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 //Db config
 const db = require('./config/keys').mongoURI;
@@ -11,6 +18,7 @@ mongoose
   
 //First route
 app.get('/', (req, res) => res.send('Hello World'));
+app.use('/api/users', users)
 
 const port = 9000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
